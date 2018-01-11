5253785473d73b20cb8e5236382101290420a693
Andres Aguilar <andresesfm@gmail.com>
2018-01-11 15:05:50 -0800

    ScrollViews

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, StyleSheet, Text, View , Image} from 'react-native';
+import { AppRegistry, StyleSheet, ScrollView, Text, View , Image, TextInput, Alert, Button} from 'react-native';
 
 class GreetingBlink extends Component {
   constructor(props){
@@ -33,20 +33,55 @@ class FixDimensionsBasics extends Component {
   }
 }
 
+class PizzaTranslator extends Component{
+  constructor(props){
+   super(props);
+   this.state = {text:''}; 
+  }
+  render(){
+    return(
+      <View style={{padding:10, width:400}}>
+        <TextInput style={{height:40}} 
+        placeholder="Type here to translate"
+        onChangeText={(text)=> this.setState({text})}/>
+        <Text style={{padding:10,fontSize:42}}>
+        {this.state.text.split(' ').map((word)=> word &&'🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }  
+}
+
+class ButtonBasics extends Component{
+  _onPressButton(){
+    Alert.alert("button tapped");
+  }
+  render(){
+    return(
+      <Button onPress={this._onPressButton} title="OKi" color="#841584" />
+    )
+  }
+}
+
 export default class HelloWorldApp extends Component {
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.red}>Open up App.jsddd to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text style={{fontSize:50}} >Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <GreetingBlink name="Test2"></GreetingBlink>
         <Image source={pic} style={{width:193, height:110}}/>
+        <PizzaTranslator />
         <FixDimensionsBasics/>
-      </View>
+        <ButtonBasics/>
+        <PizzaTranslator/>
+        <Image source={pic} style={{width:193, height:110}}/>
+        <Image source={pic} style={{width:193, height:110}}/>
+      </ScrollView>
     );
   }
 }
@@ -55,7 +90,7 @@ AppRegistry.registerComponent('AwesomeProject',()=>HelloWorldApp);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
